23047ec5a60bae984bf06099cbad1792df1dd66b
Vladislav <plavez@gmail.com>
2025-02-08 21:10:08 +0100

    starting

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_map = {}
@@ -7,4 +10,9 @@ class Solution:
                 return [num_map[complete], i]
             num_map[num] = i
         return []
-    solution = Solution()
+
+
+solution = Solution()
+nums = [2, 7, 11, 15]
+target = 9
+print(solution.twoSum(nums, target))
